9883c1ce1cd706c9e2b2c07590ca682ce65fee42
alexithan92 <alexithan92@gmail.com>
2026-03-14 15:54:58 +0800

    Update manager.py

--- CEACStatusBot/notification/manager.py
+++ CEACStatusBot/notification/manager.py
@@ -2,14 +2,11 @@ import json
 import os
 import datetime
 
-import pytz
-
 from CEACStatusBot.captcha import CaptchaHandle, OnnxCaptchaHandle
 from CEACStatusBot.request import query_status
 
 from .handle import NotificationHandle
 
-DEFAULT_ACTIVE_HOURS = "00:00-23:59"
 
 
 class NotificationManager:
@@ -29,16 +26,6 @@ class NotificationManager:
         self.__surname = surname
         self.__status_file = "status_record.json"
 
-    def _get_hour_range(self) -> list:
-        active_hours = os.getenv("ACTIVE_HOURS")
-        if active_hours is None:
-            active_hours = DEFAULT_ACTIVE_HOURS
-        start_str, end_str = active_hours.split("-")
-        start = datetime.datetime.strptime(start_str, "%H:%M").time()
-        end = datetime.datetime.strptime(end_str, "%H:%M").time()
-        if start > end:
-            raise ValueError("Start time must be before end time, got start: {start}, end: {end}")
-        return start, end
 
     def addHandle(self, notificationHandle: NotificationHandle) -> None:
         self.__handleList.append(notificationHandle)
@@ -83,28 +70,8 @@ class NotificationManager:
         with open(self.__status_file, "w") as file:
             json.dump({"statuses": statuses}, file)
 
+    
     def __send_notifications(self, res: dict) -> None:
-        if res["status"] == "Refused":
-            try:
-                TIMEZONE = os.environ["TIMEZONE"]
-                localTimeZone = pytz.timezone(TIMEZONE)
-                localTime = datetime.datetime.now(localTimeZone)
-            except pytz.exceptions.UnknownTimeZoneError:
-                print("UNKNOWN TIMEZONE Error, use default")
-                localTime = datetime.datetime.now()
-            except KeyError:
-                print("TIMEZONE Error")
-                localTime = datetime.datetime.now()
-
-            active_hour_start, active_hour_end = self._get_hour_range()
-            start_dt = datetime.datetime.combine(localTime.date(), active_hour_start, tzinfo=localTimeZone)
-            end_dt = datetime.datetime.combine(localTime.date(), active_hour_end, tzinfo=localTimeZone)
-            if not (start_dt <= localTime <= end_dt):
-                print(
-                    f"Outside active hours {os.getenv('ACTIVE_HOURS', DEFAULT_ACTIVE_HOURS)}. "
-                    "No notification sent for Refused status."
-                )
-                return
-
         for notificationHandle in self.__handleList:
             notificationHandle.send(res)
+    
